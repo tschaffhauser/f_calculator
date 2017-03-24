2a19be98953f76e21fa3826542c0eb39f9f0049f
Thomas.Schaffhauser <Thomas.Schaffhauser@IC120809.ve.local>
2017-03-24 19:23:57 +0100

    Added javadoc

--- Fakulty/src/ch/fhnw/mas/wettingen/MathUtil.java
+++ Fakulty/src/ch/fhnw/mas/wettingen/MathUtil.java
@@ -1,10 +1,15 @@
 package ch.fhnw.mas.wettingen;
 
+/**
+ * This class includes several mathematical functions.
+ * @author Alessandro Mascaro and Thomas Schaffhauser
+ */
 public class MathUtil {
-	
-	MathUtil() {
-	}
-	
+	/**
+	 * This method calculates the faculty of the input.
+	 * @param n input
+	 * @return faculty of input
+	 */
 	public static long faculty(int n) {
 		long resultat = 1;
 		for(int i = 2; i <= n ; i = i + 1 ) {
